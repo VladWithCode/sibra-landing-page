06d20ce9274823883cd832df3d7ac8877b798cc9
vladwithcode <vladwithcode@gmail.com>
2023-11-24 15:49:05 -0600

    Render View

--- frontend/main.js
+++ frontend/main.js
@@ -1,5 +1,31 @@
 // import './style.css';
+
+import loadPlan from './src/plan';
+
 document.addEventListener('DOMContentLoaded', () => {
+	// Navbar
+	const navbar = document.querySelector('#main-navbar');
+	const navbarToggler = document.querySelector('#main-navbar-toggler');
+
+	navbarToggler.addEventListener('click', () => {
+		navbar.classList.toggle('visible');
+	});
+
+	// Detail List
+	const detailList = document.querySelector('#detail-list');
+	const observer = new IntersectionObserver(
+		([entry]) => {
+			if (entry.isIntersecting) {
+				detailList
+					.querySelectorAll('li')
+					.forEach(child => child.classList.add('slideInUp'));
+			}
+		},
+		{ threshold: 0.25 }
+	);
+
+	observer.observe(detailList);
+
 	// Contact Form
 	const contactForm = document.querySelector('#contact-form');
 
@@ -31,19 +57,60 @@ document.addEventListener('DOMContentLoaded', () => {
 			if (input.tagName === 'TEXTAREA') input.innerHTML = '';
 		}
 	});
+
+	// Render / Maps
+	const locateSelectorBtns = document.querySelectorAll(
+		'[data-locate-selector]'
+	);
+	const locateDisplayElements = document.querySelectorAll(
+		'[data-locate-display]'
+	);
+	const activeBtnClasslist = [
+		'bg-white',
+		'shadow-sm',
+		'shadow-zinc-300',
+		'font-medium',
+	];
+
+	for (const btn of locateSelectorBtns) {
+		btn.addEventListener('click', () => {
+			const displayId = btn.dataset.locateSelector;
+			let displayElement;
+			btn.classList.add(...activeBtnClasslist);
+
+			for (const btn of locateSelectorBtns) {
+				const id = btn.dataset.locateSelector;
+
+				if (id !== displayId)
+					btn.classList.remove(...activeBtnClasslist);
+			}
+
+			for (const element of locateDisplayElements) {
+				const id = element.dataset.locateDisplay;
+
+				if (id === displayId) {
+					element.classList.remove('invisible');
+					displayElement = element;
+				} else {
+					element.classList.add('invisible');
+				}
+			}
+
+			if (displayId === 'render') {
+				loadPlan();
+			}
+		});
+	}
 });
 
 async function postContactRequest(requestData) {
-	const response = await fetch(
-		'http://localhost:3000/api/send-contact-request',
-		{
-			headers: {
-				'content-type': 'application/json',
-			},
-			method: 'POST',
-			body: JSON.stringify(requestData),
-		}
-	);
+	const response = await fetch('/api/send-contact-request', {
+		headers: {
+			'content-type': 'application/json',
+		},
+		method: 'POST',
+		body: JSON.stringify(requestData),
+	});
 
 	const data = await response.json();
 
--- frontend/src/plan.js
+++ frontend/src/plan.js
@@ -1,3 +1,68 @@
-import * as THREE from 'three';
+import * as Three from 'three';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
-console.log(THREE);
+/**
+ *
+ * @param {string | HTMLCanvasElement} element - The element in which the plan will be rendered
+ * @param {} options
+ */
+export default function loadPlan() {
+	const viewPortContainer = document.getElementById('plan-render-container');
+	const { height: vpHeight, width: vpWidth } =
+		viewPortContainer.getBoundingClientRect();
+
+	const loader = new GLTFLoader();
+	const scene = new Three.Scene();
+	scene.background = new Three.Color(0xffeaa7);
+
+	const camera = new Three.PerspectiveCamera(
+		75,
+		vpWidth / vpHeight,
+		0.1,
+		1000
+	);
+	camera.position.set(10, 5, 10);
+	camera.lookAt(scene.position);
+
+	const ambientLight = new Three.AmbientLight(0x404040, 0.1);
+	const hemisphericLight = new Three.HemisphereLight(0xffffbb, 0x080820, 0.8);
+	hemisphericLight.position.set(0, 430, -2500);
+
+	const renderer = new Three.WebGLRenderer();
+	renderer.setPixelRatio(window.devicePixelRatio);
+	renderer.setSize(vpWidth, vpHeight);
+	renderer.outputColorSpace = Three.SRGBColorSpace;
+	viewPortContainer.appendChild(renderer.domElement);
+
+	const controls = new OrbitControls(camera, renderer.domElement);
+	controls.maxPolarAngle = Math.PI / 2;
+	controls.addEventListener('change', () => renderer.render(scene, camera));
+	controls.update();
+
+	scene.add(ambientLight);
+	scene.add(hemisphericLight);
+
+	loader.load(
+		'/render/scene.gltf',
+		function (gltf) {
+			const model = gltf.scene;
+			scene.add(model);
+			model.scale.set(0.5, 0.5, 0.5);
+
+			controls.target.set(0, 0.5, 0);
+
+			renderer.render(scene, camera);
+		},
+		onRenderLoadProgress,
+		onRenderLoadError
+	);
+}
+
+function onRenderLoadSuccess(gltf, scene, camera, renderer) {}
+
+function onRenderLoadProgress(xhr) {}
+
+function onRenderLoadError(error) {
+	console.error(error);
+}

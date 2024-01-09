6eb11e7194417e9dd2a7450b853a4476dac689b5
VladWithCode <vladwithcode@gmail.com>
2024-01-09 16:22:52 -0600

    Remove localhost

--- frontend/main.js
+++ frontend/main.js
@@ -131,7 +131,7 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 async function postContactRequest(requestData) {
-	const response = await fetch('http://localhost:3000/api/send-contact-request', {
+	const response = await fetch('/api/send-contact-request', {
 		headers: {
 			'content-type': 'application/json',
 		},
